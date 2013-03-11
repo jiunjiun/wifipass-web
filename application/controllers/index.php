088a2cd551832ef76748a21bf30884528a9514d8
jiunjiun <quietmes@gmail.com>
2013-03-12 03:33:27 +0800

    Edit index.php test

--- application/controllers/index.php
+++ application/controllers/index.php
@@ -3,9 +3,12 @@
 class Index extends CI_Controller {
 	function __construct() {
         parent::__construct();
+		$this->load->config('config.inc');
     }
 	
 	public function index() {
 		echo 'Hello!!';
+		$this->load->model('gcm_push');
+		$this->gcm_push->push($this->config->item('Kind_StoreWifi'), 30);
 	}
 }
